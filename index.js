8aa000f6e4be943d9bb9f73ba84f2bfd6952aebd
iam-dj <6887240+iam-dj@users.noreply.github.com>
2023-04-22 23:23:19 -0700

    updates to classes and copy from UW folder

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const Employee = require('./Employee');
+const Engineer = require('./Engineer');
+const Intern = require('./Intern');
+const Manager = require('./Manager');
+
